461ba224cba498d3d2a9061d805aae662aaa75de
Peter Levart <Peter.Levart@marand.si>
2013-05-20 18:39:33 +0200

    LazyLock

---
+++ src/si/pele/concurrent/Container.java
@@ -0,0 +1,102 @@
+/*
+ * Written by Peter Levart <peter.levart@gmail.com>
+ * and released to the public domain, as explained at
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+package si.pele.concurrent;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+/**
+ * @author peter
+ */
+public class Container {
+    private static final AtomicIntegerFieldUpdater<Container> state0Updater
+        = AtomicIntegerFieldUpdater.newUpdater(Container.class, "state0");
+
+    // state0 (2 bits per lazy field, good for 1st 16 lazy fields - next 16 fields will have state1, etc...)
+    private volatile int state0;
+
+    // lock for lazyVal initialization
+    private static final LazyLock<Container> lazyValLock
+        = new LazyLock<Container>(state0Updater, 0);
+
+    // the lazy val field
+    private int lazyVal;
+
+    private int computeLazyVal() {
+        // this is the expression used to initialize lazy val
+        System.out.println(Thread.currentThread().getName() + ": computeLazyVal called");
+        try {
+            Thread.sleep(50L);
+        } catch (InterruptedException e) {}
+        return 123;
+    }
+
+    public int getLazyVal() {
+        if (lazyValLock.lock(this)) {
+            try {
+                return lazyVal = computeLazyVal();
+            } finally {
+                lazyValLock.unlock(this);
+            }
+        } else {
+            return lazyVal;
+        }
+    }
+
+
+    // test
+    public static void main(String[] args) throws Exception {
+
+        class Task implements Runnable {
+            final Container c;
+
+            Task(Container c) {
+                this.c = c;
+            }
+
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ": c.lazyVal = " + c.getLazyVal());
+            }
+        }
+
+        {
+            System.out.println("\nSingle threaded initialization...");
+            Container c = new Container();
+            new Task(c).run();
+        }
+
+        {
+            System.out.println("\nDouble threaded initialization...");
+            Container c = new Container();
+            Runnable r = new Task(c);
+            Thread t1 = new Thread(r, "t1");
+            Thread t2 = new Thread(r, "t2");
+            t1.start();
+            t2.start();
+            t1.join();
+            t2.join();
+            // final non-competing get
+            r.run();
+        }
+
+        {
+            System.out.println("\nTripple threaded initialization...");
+            Container c = new Container();
+            Runnable r = new Task(c);
+            Thread t1 = new Thread(r, "t1");
+            Thread t2 = new Thread(r, "t2");
+            Thread t3 = new Thread(r, "t3");
+            t3.start(); // 1st start t3 this time
+            t1.start();
+            t2.start();
+            t1.join();
+            t2.join();
+            t3.join();
+            // final non-competing get
+            r.run();
+        }
+    }
+}
---
+++ src/si/pele/concurrent/LazyLock.java
@@ -0,0 +1,87 @@
+/*
+ * Written by Peter Levart <peter.levart@gmail.com>
+ * and released to the public domain, as explained at
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+package si.pele.concurrent;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+/**
+ * @author peter
+ */
+public class LazyLock<T> {
+    private final AtomicIntegerFieldUpdater<T> stateUpdater;
+    private static final int state0 = 0; // uninitialized
+    private final int
+        state1, // initializing - no waiters
+        state2, // initializing - at lest one waiter
+        state3; // initialized
+
+    public LazyLock(AtomicIntegerFieldUpdater<T> stateUpdater, int lockIndex) {
+        if (lockIndex < 0 || lockIndex > 15) {
+            throw new IllegalArgumentException("lockIndex (" + lockIndex + ") should be between 0 and 15");
+        }
+        this.stateUpdater = stateUpdater;
+        state1 = 1 << (lockIndex + lockIndex);
+        state2 = state1 + state1;
+        state3 = state2 + state1;
+    }
+
+    public boolean lock(T target) {
+        for (; ; ) {
+            int state = stateUpdater.get(target);
+            int myState = state & state3;
+            if (myState == state3) {
+                // already initialized - not locking needed
+                return false;
+            } else if (myState == state0) {
+                if (stateUpdater.compareAndSet(target, state, state + state1)) {
+                    // lock established, should call unlock after initializing the lazy val
+                    return true;
+                }
+            } else if (myState == state1) {
+                // we are the 1st waiter
+                synchronized (target) {
+                    if (stateUpdater.compareAndSet(target, state, state + state1)) {
+                        try {
+                            target.wait();
+                        } catch (InterruptedException e) {}
+                    }
+                }
+            } else { // myState == state2
+                // we are the 2nd or subsequent waiter
+                synchronized (target) {
+                    if ((stateUpdater.get(target) & state3) == state2) { // recheck
+                        try {
+                            target.wait();
+                        } catch (InterruptedException e) {}
+                    }
+                }
+            }
+        }
+    }
+
+    public void unlock(T target) {
+        for (; ; ) {
+            int state = stateUpdater.get(target);
+            int myState = state & state3;
+            if (myState == state1) {
+                // still no waiters - just set state to initialized
+                if (stateUpdater.compareAndSet(target, state, state + state2)) {
+                    return;
+                }
+            } else if (myState == state2) {
+                // there are waiters - notify them after changing state to initialized
+                synchronized (target) {
+                    if (stateUpdater.compareAndSet(target, state, state + state1)) {
+                        target.notifyAll();
+                        return;
+                    }
+                }
+            } else { // myState == state0 || myState == state3
+                throw new IllegalStateException("Not locked!");
+            }
+        }
+    }
+}
